41b5e765f10c932641ec6acfc197e5c673b83549
Lucas C Settergren <lucassettergren@gmail.com>
2020-03-20 19:27:36 -0700

    Hello World + Escape Sequence

---
+++ ModifiedHelloWorld.java
@@ -0,0 +1,17 @@
+// ModifiedHelloWorld.Java
+/* Text-printing program.
+ * This comment is shown as a traditional comment. */
+
+public class ModifiedHelloWorld
+{
+  // main method begins execution of Java application
+  public static void main ( String[] args )
+  {
+    /** @return nothing
+      * @param String */
+    String text = "\"This course \n \n is awesome!\"";
+      
+    System.out.println( text );
+    
+  } // end main method
+} // end Welcome class
