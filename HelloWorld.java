d3051470920f9f76be78fd41b3d19e965735bd4e
Lucas C Settergren <lucassettergren@gmail.com>
2020-03-20 18:39:05 -0700

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// HelloWorld.Java
+/* Text-printing program.
+ * This comment is shown as a traditional comment. */
+
+public class HelloWorld
+{
+  // main method begins execution of Java application
+  public static void main ( String[] args )
+  {
+    /** @return nothing
+      * @param String */
+    System.out.println( "This course is awesome!" );
+  } // end main method
+} // end Welcome class
